ae7f3d654913f0727931bbae8dc42f7848a21f94
samuelsallee <samsallee16@gmail.com>
2022-11-01 13:05:16 +0700

    removed everything and added helloWorld

--- app/app.py
+++
@@ -1,1 +0,0 @@
-from src.helloworld import helloworld
--- layer/lambdaLayer.sh
+++
@@ -1,2 +0,0 @@
-url='https://gist.githubusercontent.com/thanakijwanavit/96b7597c0cdfc2833ddde4bccf48b180/raw/lambdaLayer.sh'
-bash <(curl -s $url)
